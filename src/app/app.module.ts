4a7a76bb41eac661e6e9262c7c43e8c6a28583c8
yujeshkc <61489344+yujeshkc@users.noreply.github.com>
2022-02-22 00:24:23 +0545

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { ProductComponent } from './component/product/product.component';
+import { CartComponent } from './component/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
